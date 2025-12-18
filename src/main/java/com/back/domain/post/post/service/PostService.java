557eab3e2aa37450ae49090fe6136e9b7e61ca91
zunobk <junho4560@naver.com>
2025-12-18 23:25:26 +0900

    validation, jpa 추가, 샘플 데이터(글 3개, 댓글 3개) 생성되도록

---
+++ src/main/java/com/back/domain/post/post/service/PostService.java
@@ -0,0 +1,50 @@
+package com.back.domain.post.post.service;
+
+import com.back.domain.post.post.entity.Post;
+import com.back.domain.post.post.repository.PostRepository;
+import com.back.domain.post.postComment.entity.PostComment;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+    private final PostRepository postRepository;
+
+    public long count() {
+        return postRepository.count();
+    }
+
+    public Post write(String title, String content) {
+        Post post = new Post(title, content);
+
+        return postRepository.save(post);
+    }
+
+    public Optional<Post> findById(int id) {
+        return postRepository.findById(id);
+    }
+
+    public List<Post> findAll() {
+        return postRepository.findAll();
+    }
+
+    public void modify(Post post, String title, String content) {
+        post.modify(title, content);
+    }
+
+    public void writeComment(Post post, String content) {
+        post.addComment(content);
+    }
+
+    public boolean deleteComment(Post post, PostComment postComment) {
+        return post.deleteComment(postComment);
+    }
+
+    public void modifyComment(PostComment postComment, String content) {
+        postComment.modify(content);
+    }
+}

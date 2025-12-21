82b23b233f29f2a1b583d672bbe980b3445224fa
zunobk <junho4560@naver.com>
2025-12-21 22:30:30 +0900

    resultCode와 msg는 조회를 제외한 모든 요청에 대한 응답의 필수요소이기 때문에 RsData 클래스 도입

--- src/main/java/com/back/domain/post/postComment/controller/ApiV1PostCommentController.java
+++ src/main/java/com/back/domain/post/postComment/controller/ApiV1PostCommentController.java
@@ -4,6 +4,7 @@ import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
 import com.back.domain.post.postComment.dto.PostCommentDto;
 import com.back.domain.post.postComment.entity.PostComment;
+import com.back.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,9 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/posts/{postId}/comments")
@@ -50,7 +49,7 @@ public class ApiV1PostCommentController {
 
     @GetMapping("/{id}/delete")
     @Transactional
-    public Map<String, Object> delete(
+    public RsData delete(
             @PathVariable int postId,
             @PathVariable int id
     ) {
@@ -60,10 +59,9 @@ public class ApiV1PostCommentController {
 
         postService.deleteComment(post, postComment);
 
-        Map<String, Object> rsData = new LinkedHashMap<>();
-        rsData.put("resultCode", "200-1");
-        rsData.put("msg", "%d번 댓글이 삭제되었습니다.".formatted(postComment.getId()));
-
-        return rsData;
+        return new RsData(
+                "200-1",
+                "댓글이 삭제되었습니다."
+        );
     }
 }
---
+++ src/main/java/com/back/global/rsData/RsData.java
@@ -0,0 +1,4 @@
+package com.back.global.rsData;
+
+public record RsData(String resultCode, String msg) {
+}

2e68fe0121f5a1fbe0425a53052c2685ffe797d9
zunobk <junho4560@naver.com>
2025-12-21 23:31:04 +0900

    RsData로 응답 데이터를 강화하고 싶었지만 RsData가 현재는 PostCommentDto 전용이라서 실패

--- src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
@@ -3,6 +3,7 @@ package com.back.domain.post.post.controller;
 import com.back.domain.post.post.dto.PostDto;
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
+import com.back.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -39,11 +40,15 @@ public class ApiV1PostController {
 
     @GetMapping("/{id}/delete")
     @Transactional
-    public PostDto delete(@PathVariable int id) {
+    public RsData delete(@PathVariable int id) {
         Post post = postService.findById(id).get();
 
         postService.delete(post);
 
-        return new PostDto(post);
+        return new RsData(
+                "200-1",
+                "%d번 글이 삭제되었습니다.".formatted(id),
+                new PostDto(post)
+        );
     }
 }

1e66ffe53c0aa47a63287565a07152946aff6fda
zunobk <junho4560@naver.com>
2025-12-25 21:50:35 +0900

    글 작성을 POST로 요청, 요청 본문은 JSON 형태로 기술

--- src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
@@ -4,6 +4,9 @@ import com.back.domain.post.post.dto.PostDto;
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
 import com.back.global.rsData.RsData;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -45,6 +48,29 @@ public class ApiV1PostController {
         return new RsData<>(
                 "200-1",
                 "%d번 글이 삭제되었습니다.".formatted(id)
+        );
+    }
+
+
+    record PostWriteForm(
+            @NotBlank
+            @Size(min = 2, max = 100)
+            String title,
+            @NotBlank
+            @Size(min = 2, max = 5000)
+            String content
+    ) {
+    }
+
+    @PostMapping
+    @Transactional
+    public RsData<PostDto> write(@Valid @RequestBody PostWriteForm form) {
+        Post post = postService.write(form.title, form.content);
+
+        return new RsData<>(
+                "200-1",
+                "%d번 글이 생성되었습니다.".formatted(post.getId()),
+                new PostDto(post)
         );
     }
 }

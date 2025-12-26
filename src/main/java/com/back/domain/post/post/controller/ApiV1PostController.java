54b3bccf088fde1a7180ce50dc051fda214708f9
zunobk <junho4560@naver.com>
2025-12-27 05:41:54 +0900

    Http 응답코드를 설정하는데는 대가가 필요하다.

--- src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
@@ -8,6 +8,8 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -70,16 +72,19 @@ public class ApiV1PostController {
 
     @PostMapping
     @Transactional
-    public RsData<PostWriteResBody> write(@Valid @RequestBody PostWriteReqBody form) {
+    public ResponseEntity<RsData<PostWriteResBody>> write(@Valid @RequestBody PostWriteReqBody form) {
         Post post = postService.write(form.title, form.content);
 
-        return new RsData<>(
-                "200-1",
-                "%d번 글이 생성되었습니다.".formatted(post.getId()),
-                new PostWriteResBody(
-                        postService.count(),
-                        new PostDto(post)
-                )
+        return new ResponseEntity<>(
+                new RsData<>(
+                        "201-1",
+                        "%d번 글이 작성되었습니다.".formatted(post.getId()),
+                        new PostWriteResBody(
+                                postService.count(),
+                                new PostDto(post)
+                        )
+                ),
+                HttpStatus.CREATED
         );
     }
 }
